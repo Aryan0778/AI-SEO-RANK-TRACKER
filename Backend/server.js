961e5328bf73f048dab3cf95c9a62e0a53aa54b0
Aryan0778 <saxenaaryan077@gmail.com>
2026-05-28 19:30:04 +0530

    middleware

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import dns from 'dns';
+import authRouter from './Routes/authRoutes.js';
 // Change DNS
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/',(req,res)=>res.send("Server is running"))
+app.use("/api/auth", authRouter)
 
 const PORT = process.env.PORT || 5000;
 
